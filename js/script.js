75301b0313ec306d298c52dd416facb9be28a530
Tomasz Spocinski <tomaszspocinski2@gmail.com>
2023-05-20 16:52:19 +0200

    adding button to toggle task done and functional

--- js/script.js
+++ js/script.js
@@ -22,14 +22,18 @@
         render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-<li
-  ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-  >
+<li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+  <button class="js-done">czy zrobione?</button>
   <button class="js-remove">usuń</button>
   ${task.content}
 </li>
@@ -44,7 +48,15 @@
                 removeTask(index);
             });
         });
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
     };
+
 
 
     const onFormSubmit = (event) => {
