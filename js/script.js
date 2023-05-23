89fb5711772d05c85ad1e70e1e60dec8a3842cd8
Tomasz Spocinski <tomaszspocinski2@gmail.com>
2023-05-23 22:38:58 +0200

    changes in code js

--- js/script.js
+++ js/script.js
@@ -1,96 +1,86 @@
 {
-    const tasks = [
-        {
-            content: "Nagrać lekcje",
-            done: false,
-        },
-        {
-            content: "Zjeść pierogi",
-            done: true,
-        },
-    ];
+    const tasks = [];
+  
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
-        render();
+      tasks.push({
+        content: newTaskContent,
+      });
+  
+      render();
     };
-
+  
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
-        render();
+      tasks.splice(taskIndex, 1);
+  
+      render();
     };
-
+  
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
-        render();
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+  
+      render();
     };
-
-    const bindEvents = () => {
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
+  
+    bindEvents = () => {
+      const removeButtons = document.querySelectorAll(".js-remove");
+  
+      removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+          removeTask(index);
         });
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-        toggleDoneButtons.forEach((toggleDoneButton, index) => {
-            toggleDoneButton.addEventListener("click", () => {
-                toggleTaskDone(index);
-            });
+      });
+  
+      const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
+  
+      toggleDoneButtons.forEach((toggleDoneButton, index) => {
+        toggleDoneButton.addEventListener("click", () => {
+          toggleTaskDone(index);
         });
-
+      });
     };
-
+  
     const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-<li class="tasks__item js-task">
-<button class="js-done">
-        ${task.done ? "✔" : ""} 
-        </button>
-<span class="tasks-content${task.done ? " tasks__content--done" : ""}">
-        ${task.content} 
-</span>
-<button class="js-remove">🗑</button>
-</li>
-`;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
-        bindEvents();
+      let htmlString = "";
+  
+      for (const task of tasks)
+        htmlString += `
+      <li class="task__items">
+          <button class="task__button task__button--toggleDone js-toggleDone">
+            ${task.done ? "✓" : ""}
+          </button>
+          <span class="task__text ${task.done ? "task__text--done" : ""}">
+            ${task.content}
+          </span>
+          <button class="task__button task__button--remove js-remove">🗑</button>
+      </li>
+  `;
+  
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+  
+      bindEvents();
     };
-
-
-
+  
     const onFormSubmit = (event) => {
-        event.preventDefault();
-
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-        if (newTaskContent === "") {
-            return;
-        }
-
+      event.preventDefault();
+  
+      const newTaskInput = document.querySelector(".js-newTask");
+      const newTaskContent = newTaskInput.value.trim();
+  
+      if (newTaskContent !== "") {
         addNewTask(newTaskContent);
+      }
+  
+      newTaskInput.value = "";
+      newTaskInput.focus();
     };
-
-
+  
     const init = () => {
-        render();
-
-
-        const form = document.querySelector(".js-form");
-
-        form.addEventListener("submit", onFormSubmit);
+      render();
+  
+      const form = document.querySelector(".js-form");
+  
+      form.addEventListener("submit", onFormSubmit);
     };
-
+  
     init();
-
-};
-
+  }
